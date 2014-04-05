2c260f4a30ad11b34c99014b9c05399c1c68208d
Ben Pollock <B.O.Pollock@gmail.com>
2014-04-04 20:08:49 -0400

    boundary destruction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, yMin, yMax;
+}
+
 public class PlayerController : MonoBehaviour 
 {
 
 	public float speed;
+	public Boundary boundary;
 
 	void FixedUpdate()
 	{
@@ -13,6 +20,9 @@ public class PlayerController : MonoBehaviour
 
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		rigidbody2D.velocity = movement * speed;
+
+		rigidbody2D.transform.position = new Vector2 (Mathf.Clamp (rigidbody2D.transform.position.x, boundary.xMin, boundary.xMax), Mathf.Clamp (rigidbody2D.transform.position.y, boundary.yMin, boundary.yMax));
+	
 	}
 
 }
