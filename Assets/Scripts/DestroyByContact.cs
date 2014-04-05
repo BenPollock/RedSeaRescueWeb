d36d878e000b66c34b739a83e18424a25f666289
Ben Pollock <B.O.Pollock@gmail.com>
2014-04-04 21:33:26 -0400

    Waves, score text implemented

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,11 +3,24 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameController gameController;
+
+	void Start()
+	{
+		//find the game controller object so we can modify the score
+		/*GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) 
+		{
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}*/
+	}
+
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		if (other.tag == "Boundary") {
 			return;
 		}
+		//	gameController.AddScore (1000000);
 			Destroy (other.gameObject); //destroy player, we'll fix this later
 			Destroy (gameObject);  //destroy itself
 		}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	public GUIText scoreText;
+	private int score;
+
+	void Start()
+	{
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves());
+		StartCoroutine (ScoreAdder ());
+	}
+
+	IEnumerator SpawnWaves () //ienumerator for asynchronous
+	{
+		yield return new WaitForSeconds (startWait);
+		while(true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, 0.0f);
+				Quaternion spawnRotation = new Quaternion (0.0f, 0.0f, 0.0f, 0.0f);
+				Instantiate(hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
+	}
+
+	IEnumerator ScoreAdder ()
+	{
+		while (true) //TODO: change to as long as player still alive
+		{
+			yield return new WaitForSeconds (0.1f);
+			score += 100;
+			UpdateScore ();
+		}
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	//Add score from other actions
+	public void AddScore(int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+}
