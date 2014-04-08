cfd3023adb0448c573ab1b8e9938855e54202d6a
Ben Pollock <B.O.Pollock@gmail.com>
2014-04-08 13:39:19 -0400

    admob for iOS

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -307,10 +307,13 @@ public class GameController : MonoBehaviour
 			PlayerPrefs.SetInt("Highscore", score);
 		}
 
+		AdMobPlugin.ShowBannerView();
+
 		//Create game over popup
 		Quaternion spawnRotation = new Quaternion (0.0f, 0.0f, 0.0f, 0.0f);
 		Vector3 spawnPosition = new Vector3 (0f, 1.51f, 0.0f);
 		_gameOverBox = (GameObject)Instantiate (gameOverBox, spawnPosition, spawnRotation);
+
 
 
 
@@ -327,10 +330,12 @@ public class GameController : MonoBehaviour
 			//Button to play again
 			GUI.backgroundColor = Color.clear;
 			if (GUI.Button (new Rect (100, Screen.height - (int)(Screen.height / 10) * 6, Screen.width - 200, (int)(Screen.height / 10)), replay)) {
-					Application.LoadLevel ("main");
+				Application.LoadLevel ("main");
+				AdMobPlugin.HideBannerView();
 			}
 			if (GUI.Button (new Rect (100, Screen.height - (int)(Screen.height / 10) * 5, Screen.width - 200, (int)(Screen.height / 10)), quit)) {
 				Application.LoadLevel ("splash");
+				AdMobPlugin.HideBannerView();
 			}
 		}
 	}
